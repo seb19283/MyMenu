f47ba621a12786c3f24e6826d6f99a420dfabf8c
Sebastian Connelly (student LM) <s014443@lm-18s014443.lmsd.org>
2018-02-14 12:12:32 -0500

    Initial Commit

---
+++ MyMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMenu
+//
+//  Created by Sebastian Connelly (student LM) on 2/14/18.
+//  Copyright © 2018 Sebastian Connelly (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
