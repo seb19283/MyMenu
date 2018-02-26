69ae5bf0cd5b626ab0eadd20283f81b4fc8d561f
Isaac Rand (student LM) <s021521@lm-18s021521.lmsd.org>
2018-02-26 14:03:00 -0500

    Ingredients Class

---
+++ MyMenu/Ingredient.swift
@@ -0,0 +1,21 @@
+//
+//  Ingredient.swift
+//  MyMenu
+//
+//  Created by Isaac Rand (student LM) on 2/22/18.
+//  Copyright © 2018 Sebastian Connelly (student LM). All rights reserved.
+//
+
+import Foundation
+class Ingredient{
+    
+    var name: String = ""
+    var category: Int = 0
+
+    init(name: String, category: Int){
+        self.name = name
+        self.category = category
+    }
+    
+    
+}
