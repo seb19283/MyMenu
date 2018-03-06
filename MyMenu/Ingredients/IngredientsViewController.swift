8ed0071c4c4f2abd332ae3f3561a1359728a1785
Sebastian Connelly (student LM) <s014443@lm-18s014443.lmsd.org>
2018-03-06 11:53:27 -0500

    Drop down menu

--- MyMenu/Ingredients/IngredientsViewController.swift
+++ MyMenu/Ingredients/IngredientsViewController.swift
@@ -16,39 +16,44 @@ class IngredientsViewController: UIViewController, UITableViewDataSource, UITabl
     @IBOutlet var cancelButton: UIBarButtonItem!
     @IBOutlet var navigationBar: UINavigationItem!
     
-    var ingredients: [[String]] = [[String]]()
+    var ingredients = [ExpandableNames]()
     var ref: DatabaseReference!
     var databaseHandle: DatabaseHandle?
     var addButton: UIBarButtonItem!
+    var categories = [String]()
+    var open: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         ref = Database.database().reference()
         
-        tableView.register(IngredientCell.self, forCellReuseIdentifier: "cellID")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+        tableView.dataSource = self
+        tableView.delegate = self
         
         addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonClicked))
         
         navigationBar.leftBarButtonItem = nil
         
-        getIngredients()
+        getCategories()
     }
     
     @objc func addButtonClicked(){
         
     }
     
-    func getIngredients(){
-        ref.child((UIDevice.current.identifierForVendor?.uuidString)!).child("Ingredients").observe(.value, with: { (snapshot) in
+    func getCategories(){
+        ref.child((UIDevice.current.identifierForVendor?.uuidString)!).child("Ingredients").observe(.childAdded, with: { (snapshot) in
             
-            if let dictionary = snapshot.value as? [String:[String]] {
-                
-                for (_, ingredient) in dictionary{
-                    self.ingredients.append(ingredient)
-                }
-                
+            let category = snapshot.key
+            self.categories.append(category)
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+//                self.ingredients.append(ExpandableNames(isExpanded: true, names: names))
+                self.tableView.reloadData()
             }
+            
             
         }, withCancel: nil)
     }
@@ -72,23 +77,82 @@ class IngredientsViewController: UIViewController, UITableViewDataSource, UITabl
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return ingredients.count
+        return categories.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ingredients[section].count
+        if ingredients[section].isExpanded {
+            return ingredients[section].names.count
+        } else {
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID") as! IngredientCell
-        
-        
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID") as! UITableViewCell
+        cell.textLabel?.text = ingredients[indexPath.section].names[indexPath.row]
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
         
+        view.backgroundColor = UIColor.lightGray
+        
+        let categoryLabel: UILabel = {
+            let label = UILabel()
+            label.text = categories[section]
+            label.textColor = .black
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.font = UIFont(name: "Helvetica Neue", size: 16)
+            return label
+        }()
+        
+        let arrowImageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            return imageView
+        }()
+        
+        view.addSubview(arrowImageView)
+        arrowImageView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        arrowImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        arrowImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        arrowImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        view.addSubview(categoryLabel)
+        categoryLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        categoryLabel.rightAnchor.constraint(equalTo: arrowImageView.leftAnchor).isActive = true
+        categoryLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        categoryLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        view.tag = section
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(expandHeader(sender:))))
+        
+        return view
+    }
+    
+    @objc func expandHeader(sender: UITapGestureRecognizer){
+        
+        guard let section = sender.view?.tag else {
+            return
+        }
+        
+        var indexPaths = [IndexPath]()
+        for row in ingredients[section].names.indices {
+            let indexPath = IndexPath(row: row, section: section)
+            indexPaths.append(indexPath)
+        }
+        
+        let isExpanded = ingredients[section].isExpanded
+        ingredients[section].isExpanded = !isExpanded
+        
+        if(isExpanded){
+            tableView.deleteRows(at: indexPaths, with: .fade)
+        } else {
+            tableView.insertRows(at: indexPaths, with: .fade)
+        }
     }
     
     func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
@@ -103,19 +167,4 @@ class IngredientsViewController: UIViewController, UITableViewDataSource, UITabl
         return swipeAction
     }
     
-}
-
-//TableViewCell class
-class IngredientCell: UITableViewCell {
-    
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 }
---
+++ MyMenu/Structs/ExpandableNames.swift
@@ -0,0 +1,14 @@
+//
+//  ExpandableNames.swift
+//  MyMenu
+//
+//  Created by Sebastian Connelly (student LM) on 3/3/18.
+//  Copyright © 2018 Sebastian Connelly (student LM). All rights reserved.
+//
+
+import Foundation
+
+struct ExpandableNames {
+    var isExpanded: Bool
+    let names: [String]
+}
